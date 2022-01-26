29bf7e0ef523d092871ceec982e3030662049a56
Paulão <45020867+PauloRibeiroJr@users.noreply.github.com>
2022-01-26 18:45:13 -0300

    CORS habilita conexão

--- index.js
+++ index.js
@@ -109,10 +109,9 @@ async function main() {
         res.send("Item removido com sucesso.");
     });
     //Permitir que outra app recuper dados
-    app.use((req,res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin',"*");
-    res.setHeader('Access-Control-Allow-Headers',"*");
-    res.header('Access-Control-Allow-Credentials', true);
+    app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
     });
 
