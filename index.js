391abf01a2c9bf29c5d050e9c1d10169a6b87d79
Paulão <45020867+PauloRibeiroJr@users.noreply.github.com>
2022-01-26 18:38:31 -0300

    Permitir outro app consumir

--- index.js
+++ index.js
@@ -108,6 +108,13 @@ async function main() {
 
         res.send("Item removido com sucesso.");
     });
+    //Permitir que outra app recuper dados
+    app.use((req,res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin',"*");
+    res.setHeader('Access-Control-Allow-Headers',"*");
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
     app.listen(process.env.PORT || 3000);
 }
