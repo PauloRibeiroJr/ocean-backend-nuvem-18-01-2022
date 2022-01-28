9dbae98e6907ebccf7b2c548af0b6b4adbd620f1
PauloRibeiroJr <paulo.ribeirojr@gmail.com>
2022-01-28 15:54:16 -0300

    Implementando seguraça

--- index.js
+++ index.js
@@ -15,108 +15,116 @@ const url = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}`;
 //const dbName = "ocean_bancodados_18_01_2022";
 //const dbName = "ocean_bkend_20220118";
 
-
 async function main() {
-    // Conexão com o Banco de Dados
+  // Conexão com o Banco de Dados
 
-    const client = await MongoClient.connect(url);
+  const client = await MongoClient.connect(url);
 
-    const db = client.db(dbName);
+  const db = client.db(dbName);
 
-    const collection = db.collection("herois");
+  const collection = db.collection("herois");
 
-    //const collection = undefined;
-    // Aplicação em Express
+  //const collection = undefined;
+  // Aplicação em Express
 
-    const app = express();
+  const app = express();
 
-    // Sinalizo para o Express que o body das requisições
-    // estará sempre estruturado em JSON
-    app.use(express.json());
+  // Sinalizo para o Express que o body das requisições
+  // estará sempre estruturado em JSON
+  app.use(express.json());
 
-    // Endpoint "/"
-    app.get("/", function (req, res) {
-        res.send("Hello, World!");
-    });
+  // Endpoint "/"
+  app.get("/", function (req, res) {
+    res.send("Hello, World!");
+  });
 
-    // Endpoint "/oi"
-    app.get("/oi", function (req, res) {
-        res.send("Olá, mundo!");
-    });
+  // Endpoint "/oi"
+  app.get("/oi", function (req, res) {
+    res.send("Olá, mundo!");
+  });
 
-    app.get("/oi/:name", async function (req, res) {
-        const nome = req.params.name;
-        
-        const item = {"nome": nome };
-        
-        await collection.insertOne(item);
-        
-        res.send("Olá, "+ nome);
-    });    
-    
-    const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
-    //              0                   1                2
+  app.get("/oi/:name", async function (req, res) {
+    const nome = req.params.name;
 
-    // [GET] "/herois" - Read All (Ler todos os registros)
-    app.get("/herois", async function (req, res) {
-        res.set("Access-Control-Allow-Origin", "*");        
-        const documentos = await collection.find().toArray();
+    const item = { nome: nome };
 
-        res.send(documentos);
-    });
+    await collection.insertOne(item);
 
-    // [GET] "/herois/:id" - Read Single (by Id) (Ler um registro pelo ID)
-    app.get("/herois/:id", async function (req, res) {
-        const id = req.params.id;
+    res.send("Olá, " + nome);
+  });
 
-        const item = await collection.findOne({ _id: new ObjectId(id) });
+  const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+  //              0                   1                2
 
-        res.send(item);
-    });
+  // [GET] "/herois" - Read All (Ler todos os registros)
+  app.get("/herois", async function (req, res) {
+    if (
+      !req.headers.authorization ||
+      req.headers.authorization.indexOf("Basic ") === -1
+    ) {
+      return res.status(401).json({ message: "Missing Authorization Header" });
+    }
+    //res.set("Access-Control-Allow-Origin", "*");
+    const documentos = await collection.find().toArray();
 
-    // [POST] "/herois" - Create (Criar um registro)
-    app.post("/herois", async function (req, res) {
-        const item = req.body;
-        
-        res.send(item);
-        
-        await collection.insertOne(item);
+    res.send(documentos);
+  });
 
-        //res.send(item);
-    });
+  // [GET] "/herois/:id" - Read Single (by Id) (Ler um registro pelo ID)
+  app.get("/herois/:id", async function (req, res) {
+    const id = req.params.id;
 
-    // [PUT] "/herois/:id" - Update (Atualizar um registro)
-    app.put("/herois/:id", async function (req, res) {
-        const id = req.params.id;
+    const item = await collection.findOne({ _id: new ObjectId(id) });
 
-        const item = req.body;
+    res.send(item);
+  });
 
-        await collection.updateOne(
-            { _id: new ObjectId(id) },
-            {
-                $set: item,
-            }
-        );
+  // [POST] "/herois" - Create (Criar um registro)
+  app.post("/herois", async function (req, res) {
+    const item = req.body;
 
-        res.send(item);
-    });
+    res.send(item);
 
-    // [DELETE] "/herois/:id" - Delete (Remover um registro)
-    app.delete("/herois/:id", async function (req, res) {
-        const id = req.params.id;
+    await collection.insertOne(item);
 
-        await collection.deleteOne({ _id: new ObjectId(id) });
+    //res.send(item);
+  });
 
-        res.send("Item removido com sucesso.");
-    });
-    //Permitir que outra app recuper dados
-    app.use(function(req, res, next) {
+  // [PUT] "/herois/:id" - Update (Atualizar um registro)
+  app.put("/herois/:id", async function (req, res) {
+    const id = req.params.id;
+
+    const item = req.body;
+
+    await collection.updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $set: item,
+      }
+    );
+
+    res.send(item);
+  });
+
+  // [DELETE] "/herois/:id" - Delete (Remover um registro)
+  app.delete("/herois/:id", async function (req, res) {
+    const id = req.params.id;
+
+    await collection.deleteOne({ _id: new ObjectId(id) });
+
+    res.send("Item removido com sucesso.");
+  });
+  //Permitir que outra app recuper dados
+  app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
-    });
+  });
 
-    app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000);
 }
 
 main();
